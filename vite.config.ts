b226bec94d2d0e20915ce2b4791edc06df959976
Luiz Ricardo Brugnera <luiz.r.brugnera@gmail.com>
2024-04-24 18:43:02 -0300

    fix: development test config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://18.229.150.86:5173", // URL do seu back-end HTTP
+        changeOrigin: true,
+        secure: false,
+        rewrite: (path) => path.replace(/^\/api/, ""),
+      },
+    },
+  },
+});
